d679d2aacf1bd8623dca734f727fb3d7930bdc1f
netanelHAsgardSystems <netanel.h@asgardsys.co.il>
2022-07-27 13:59:10 +0300

    before babel

---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+import InputManager from "./InputManager";
+import { vibrate } from "./services/vibrate";
+import { getPlatform } from "./services/platform";
+import VirtualJoystick from "./components/VirtualJoystick";
+
+export { InputManager, vibrate, getPlatform, VirtualJoystick };
